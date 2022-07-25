78a43da443d42fad97476e74d59b53b96dfb7f16
Ronia <roni.rebane@gmail.com>
2022-07-25 13:53:51 +0300

    fix: DefaultPaths.php

--- src/Implementation/DefaultPaths.php
+++ src/Implementation/DefaultPaths.php
@@ -6,5 +6,5 @@ namespace Quenary\Implementation;
 
 class DefaultPaths extends Paths
 {
-    public const YAML_CONFIG = __DIR__ . '/tmp/eb1bf189-11d7-48ec-94ea-2cf07a36e782.eventbus.yaml';
+    public const YAML_CONFIG = './tmp/eb1bf189-11d7-48ec-94ea-2cf07a36e782.eventbus.yaml';
 }
